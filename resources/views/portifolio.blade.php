32b50f0551e5fe6255ae2e42bef0ed65df32796f
leandrosilvarmos <leandro.silva.rmos@gmail.com>
2024-03-15 18:08:51 -0300

    Parte do navigation estruturada feita somente o html

---
+++ resources/views/portifolio.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+</head>
+
+<body>
+    <div class="navigation">
+        <nav class="navigation-header">
+            <ul>
+                <li><a href="#">APRESENTAÇÃO</a></li>
+                <li><a href="#">TECNOLOGIAS</a></li>
+                <li><a href="#">PROJETOS</a></li>
+                <li><a href="#">EXPERIENCIAS</a></li>
+                <li><a href="#">FEEDBACKS</a></li>
+            </ul>
+        </nav>
+
+        <div class="social">
+            <div class="social-content">
+                <a href="" class="social-content image">
+                    <spam class="social-content text-subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</spam>
+                </a>
+            </div>
+            <div class="social-content">
+                <a href="" class="social-content image">
+                    <img src="" alt="">
+                    <spam class="social-content text-subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</spam>
+                </a>
+            </div>
+            <div class="social-content">
+                <a href="" class="social-content image">
+                    <spam class="social-content text-subtitles">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</spam>
+                </a>
+            </div>
+        </div>
+
+        <section class="about-me">
+            <div class="about-me image"><img src="" alt="imagem pessoal"></div>
+            <div class="about-me content">
+                <spam class="about-me content text-title">Leandro Ramos Silva</spam>
+                <spam class="about-me content text-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Mollitia
+                    dolores, labore consequatur porro iure tempore voluptatibus officia laboriosam atque aliquid minima
+                    cum dolore fugit velit consequuntur ea omnis obcaecati placeat?</spam>
+            </div>
+
+        </section>
+    </div>
+</body>
+
+</html>
