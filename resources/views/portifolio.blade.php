44b1db76f57011cb80fa26fb6eff37508d5c598b
leandrosilvarmos <leandro.silva.rmos@gmail.com>
2024-03-19 17:40:50 -0300

    Atualizacoes no navigation e na estrutura da parte de social do header

--- resources/views/portifolio.blade.php
+++ resources/views/portifolio.blade.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title></title>
 </head>
 
 <body>
     <div class="navigation">
-        <nav class="navigation-header">
-            <ul>
+        <nav>
+            <ul class="navigation-header">
                 <li><a href="#">APRESENTAÇÃO</a></li>
                 <li><a href="#">TECNOLOGIAS</a></li>
                 <li><a href="#">PROJETOS</a></li>
@@ -24,20 +26,29 @@
         <div class="social">
             <div class="social-content">
                 <a href="" class="social-content image">
-                    <spam class="social-content text-subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</spam>
+                    <img src="" class="fa fa-brands fa-github" alt="">
+                    <span class="social-content text-title"> Projetos</span>
+                    <spam class="social-content text-subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    </spam>
                 </a>
             </div>
             <div class="social-content">
                 <a href="" class="social-content image">
-                    <img src="" alt="">
-                    <spam class="social-content text-subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</spam>
+                    <img src="" class="fa fa-brands fa-linkedin-in" alt="">
+                    <span class="social-content text-title">Rede de Contatos</span>
+                    <spam class="social-content text-subtitles">Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit.</spam>
                 </a>
             </div>
             <div class="social-content">
                 <a href="" class="social-content image">
-                    <spam class="social-content text-subtitles">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</spam>
+                    <img src="" class="fa fa-solid fa-comments-dollar" alt="">
+                    <span class="social-content text-title">Orçamentos</span>
+                    <spam class="social-content text-subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    </spam>
                 </a>
             </div>
+
         </div>
 
         <section class="about-me">
@@ -52,6 +63,10 @@
 
         </section>
     </div>
+
+    <section>
+
+    </section>
 </body>
 
 </html>
