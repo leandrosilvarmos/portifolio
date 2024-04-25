7abe52f2d584d34ef4947ee5918b5301f5686876
leandrosilvarmos <leandro.silva.rmos@gmail.com>
2024-04-25 12:08:43 -0300

    Atualizando o README.me e tambem fazendo o deploy pro heroku

--- resources/views/portifolio.blade.php
+++ resources/views/portifolio.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="https://portifolio-93179d6d0b02.herokuapp.com/css/reset.css">
-    <link rel="stylesheet" href="https://portifolio-93179d6d0b02.herokuapp.com/css/style.css"> --}}
+    <link rel="stylesheet" href="https://portifolio-93179d6d0b02.herokuapp.com/css/reset.css">
+    <link rel="stylesheet" href="https://portifolio-93179d6d0b02.herokuapp.com/css/style.css">
 
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -338,10 +338,10 @@
         AOS.init();
     </script>
 
-    <script type="text/javascript" src="{{ asset('js/particle.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
-    {{-- <script type="text/javascript" src="https://portifolio-93179d6d0b02.herokuapp.com/js/particle.js"></script>
-    <script type="text/javascript" src="https://portifolio-93179d6d0b02.herokuapp.com/js/script.js"></script> --}}
+    {{-- <script type="text/javascript" src="{{ asset('js/particle.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/script.js') }}"></script> --}}
+    <script type="text/javascript" src="https://portifolio-93179d6d0b02.herokuapp.com/js/particle.js"></script>
+    <script type="text/javascript" src="https://portifolio-93179d6d0b02.herokuapp.com/js/script.js"></script>
 
 
     <div class="up-page" id="up-page">
